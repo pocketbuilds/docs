e455d112073f13702a11552a7a6252aebc3b39a6
steve <sjmacleoddev@gmail.com>
2025-09-04 11:31:35 -0400

    Home Page content

--- layouts/content/_index.svelte
+++ layouts/content/_index.svelte
@@ -1,10 +1,16 @@
 <script>
 	import DocsPager from "../widgets/docs_pager.svelte";
-	export let title, docsManager;
+	export let title, content, components, allLayouts, allContent, docsManager;
 </script>
 
 <section>
 	<h1>{title}</h1>
+
+	{#if components?.length}
+		{#each components as component }
+			<svelte:component this="{allLayouts["layouts_components_" + component.name + "_svelte"]}" {...component} {content} {allContent} {allLayouts} />
+		{/each}
+	{/if}
 
 	<DocsPager {docsManager} />
 </section>
