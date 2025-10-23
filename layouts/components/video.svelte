830131f5169652ef039591589b9daa515b5a1146
jimafisk <jim.fisk@jantcu.com>
2025-10-23 16:47:37 -0400

    Add intro video.

---
+++ layouts/components/video.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let video;
+</script>
+
+<video width="640" height="360" controls preload="metadata">
+	<source src="/media/videos/{video}.mp4" type="video/mp4">
+	Your browser doesn't support HTML5 video. Download it <a href="/videos/my-demo.mp4">here</a>.
+</video>
+
+<style>
+	video {
+		max-width: 100%;
+		height: auto;
+		margin: 10px 0;
+	}
+</style>
